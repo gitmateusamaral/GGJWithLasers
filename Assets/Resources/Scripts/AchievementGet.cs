a0a251caa59d2f57f76160986103815f6ed3a830
Matheus Avellar <matheusavellarb@gmail.com>
2016-01-31 01:12:43 -0200

    Wasn't even going to commit, but whatever

---
+++ Assets/Resources/Scripts/AchievementGet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementGet : MonoBehaviour {
+
+	Vector2 targetPosition1;
+	Vector2 targetPosition2;
+	int gambiarra;
+
+	void Start () {
+		targetPosition1 = new Vector2 (this.transform.localPosition.x, 33f);
+		targetPosition2 = new Vector2 (this.transform.localPosition.x, -170f);
+		gambiarra = 0;
+	}
+
+	void Update () {
+		if (this.transform.localPosition.y != targetPosition1.y && gambiarra == 0) {
+			this.transform.localPosition = Vector2.MoveTowards (this.transform.localPosition, targetPosition1, 400 * Time.deltaTime);
+		} else if (gambiarra < 500) {
+			gambiarra++;
+		} else {
+			this.transform.localPosition = Vector2.MoveTowards (this.transform.localPosition, targetPosition2, 400 * Time.deltaTime);
+		}
+	}
+}
--- Assets/Resources/Scripts/GameController.cs
+++ Assets/Resources/Scripts/GameController.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
-	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
 	}
 }
